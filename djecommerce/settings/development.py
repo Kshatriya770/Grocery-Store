63451f0f796276797b966ba677d25b475b050bd9
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2020-01-11 14:09:28 +0530

    Initial commit

---
+++ djecommerce/settings/development.py
@@ -0,0 +1,36 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar'
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+# DEBUG TOOLBAR SETTINGS
+
+DEBUG_TOOLBAR_PANELS = [
+
+]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STRIPE_PUBLIC_KEY = ''
+STRIPE_SECRET_KEY = ''
